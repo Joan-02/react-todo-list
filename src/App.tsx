c8bdc9bd60bb511a75b934454758a8d8430a58a8
Joan Sanchis <jsalvadorsb@gmail.com>
2025-03-28 10:35:26 +0100

    todo list mejorada pero no terminada

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,47 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import './App.css'
-
+interface Task {
+  text: string;
+  isCompleted: boolean;
+}
 
 function App() {
+  
+  const [tasks, setTask] = useState<Task[]>([]);
+  const [taskText, setTaskText] = useState('');
 
-  // const [taskArray, setTaskArray] = useState([]);
-  // const [taskText, setTaskText] = useState([]);
+  const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskText(event.target.value)
+  };
 
-  const taskArray = ['task1', 'task2', 'task3'];
+  console.log(taskText);
 
-  // const handleTask = () => {
-  //   setTaskArray(taskText);
-
-  //   console.log(setTaskArray);
-  // };
-
-  // const handleText = () => {
-  //   setTaskText(taskText);
-
-  //   console.log(setTaskText);
-  // };
+  const handleTask = () => {
+    setTask([...tasks, { text: taskText, isCompleted: false}]);
+    setTaskText('');
+  };
 
   return (
     <>
       <h1>ToDo List</h1>
-      <input type="text" />
-      <button>Add Task</button>
-      <ul>
-        <li>
-
-        </li>
-      </ul>
+      <div className='add-task'>
+        <input type="text" onChange={handleText} value={taskText}/>
+        <button onClick={handleTask}>Add Task</button>
+      </div>
+      <div className='task-list'>
+        {tasks.map((task) => {
+          return (
+            <div className='task'>
+              <div>
+                <input type="checkbox" checked={task.isCompleted}/>
+                <span>{task.text}</span>
+              </div>
+              <button>Eliminar</button>
+            </div>
+          );
+        })};
+        
+      </div>
     </>
   )
 }
