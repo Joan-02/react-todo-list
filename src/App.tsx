5bdb1b37b2016c5e92e0e729137ca2dda28e9c15
Joan Sanchis <jsalvadorsb@gmail.com>
2025-04-02 09:16:20 +0200

    functions delete task and check task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 interface Task {
   text: string;
   isCompleted: boolean;
@@ -7,44 +7,65 @@ interface Task {
 }
 
 function App() {
-  
   const [tasks, setTask] = useState<Task[]>([]);
-  const [taskText, setTaskText] = useState('');
+  const [taskText, setTaskText] = useState("");
 
   const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTaskText(event.target.value)
+    setTaskText(event.target.value);
   };
 
   console.log(taskText);
 
   const handleTask = () => {
-    setTask([...tasks, { text: taskText, isCompleted: false, id: Date.now()}]);
-    setTaskText('');
+    setTask([...tasks, { text: taskText, isCompleted: false, id: Date.now() }]);
+    setTaskText("");
+  };
+
+  const handleDelete = (id: number) => {
+    setTask(tasks.filter((task) => task.id !== id));
+  };
+
+  const handleCheckbox = (id: number) => {
+    setTask(
+      tasks.map((task) =>
+        task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+      )
+    );
   };
 
   return (
     <>
       <h1>ToDo List</h1>
-      <div className='add-task'>
-        <input type="text" onChange={handleText} value={taskText}/>
+      <div className="add-task">
+        <input type="text" onChange={handleText} value={taskText} />
         <button onClick={handleTask}>Add Task</button>
       </div>
-      <div className='task-list'>
+      <div className="task-list">
         {tasks.map((task) => {
           return (
-            <div className='task'>
+            <div className="task">
               <div>
-                <input type="checkbox" checked={task.isCompleted}/>
-                <span>{task.text}</span>
+                <input
+                  type="checkbox"
+                  checked={task.isCompleted}
+                  onChange={() => handleCheckbox(task.id)}
+                />
+                <span className={task.isCompleted ? "completed" : ""}>
+                  {task.text}
+                </span>
               </div>
-              <button className='eliminar-task'>Eliminar</button>
+              <button
+                className="eliminar-task"
+                onClick={() => handleDelete(task.id)}
+              >
+                Eliminar
+              </button>
             </div>
           );
-        })};
-        
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
